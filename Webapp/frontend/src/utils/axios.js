c9f797f2510bfd912d6a859d9f5794ca0a9a93f4
Sarthak-Sama <sarthak.saklani04@gmail.com>
2025-02-06 01:53:31 +0530

    changed the axios endpoint

--- Webapp/frontend/src/utils/axios.js
+++ Webapp/frontend/src/utils/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const instance = axios.create({
-  // baseURL: "https://codetracker-backend-fc9r.onrender.com/",
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://codetracker-backend-fc9r.onrender.com/",
+  // baseURL: "http://localhost:3000/",
   headers: {
     accept: "application/json",
   },
